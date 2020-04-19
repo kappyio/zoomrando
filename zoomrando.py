3b833e2dd1ff74c879e4f78804fd8948c63e5b38
David Kaplowitz <shredneckjazz@gmail.com>
2020-04-19 13:54:17 -0400

    using a list instead of a set. generates them all, then prints them all out in a more friendly way

--- zoomrando.py
+++ zoomrando.py
@@ -16,18 +16,26 @@ screens = (math.ceil(participants / (rows * cols)))
 # r_participants = input("How many partcipants are there? ")
 # r_row = input("How many rows are there? ")
 # r_col = input("How many columns are there? ")
-my_set = set()
+# my_set = set()
+my_list = list()
 
-print("There are {0} screens with {1} rows and {2} columns.".format(screens, rows, cols))
+print("There are {0} participants spreade across {1} screens with {2} rows and {3} columns.".format(participants, screens, rows, cols))
 
 
-while len(my_set) < participants:
+while len(my_list) < participants:
     r_scr = random.randint(1, screens)
     r_row = random.randint(1, rows)
     r_col = random.randint(1, cols)
     my_digits = int(str(r_scr) + str(r_row) + str(r_col))
-    # print(type(my_digits))
     # print(my_digits)
-    my_set.add(my_digits)
+    if my_digits in my_list:
+        continue
+    else:
+        my_list.append(my_digits)
 
-print(my_set)
+# print("This meetings has {0} participants.".format(len(my_list)))
+
+# print(my_list)
+
+for num in my_list:
+    print("Screen: {0}, Row: {1}, Column: {2}".format(str(num)[0], str(num)[1], str(num)[2]))

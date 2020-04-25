f41351e8f4e53b9aceccf9121b1dfc52118d6aa1
David Kaplowitz <shredneckjazz@gmail.com>
2020-04-25 08:06:46 -0400

    merging my_argp with zoomrando

--- zoomrando.py
+++ zoomrando.py
@@ -1,48 +1,43 @@
-import random
-import math
+#!/usr/bin/env python3
 
 # TODO
-# prompt, or run with args
-# Make sure an int is entered
-# Limit to n participants (300)
+# Limit to n participants (500)
 # Make into a web page people can go to to use
 
-# BUGS
-# Nice if everything is three digits. But what about screens of 10 or more?
+import argparse
+import math
+import random
 
+parser = argparse.ArgumentParser(description='Produce a list of random grid \
+                                 coordinates for participants in a zoom meeting. \
+                                 Uses Zoom\'s "Gallery View".')
+parser.add_argument('participants', type=int, help="How many participants are in the meeting?")
+parser.add_argument('rows', type=int, help="How many rows do you see in gallery view?")
+parser.add_argument('cols', type=int, help="How many columns do you see in gallery view?")
+args = parser.parse_args()
 
-participants = 300
-rows = 5
-cols = 4
-screens = (math.ceil(participants / (rows * cols)))
+def grid_calc(participants, rows, cols):
+    my_list = list()
+    screens = (math.ceil(participants / (rows * cols)))
+    while len(my_list) < participants:
+        r_scr = random.randint(1, screens)
+        r_row = random.randint(1, rows)
+        r_col = random.randint(1, cols)
+        my_digits = int(str(r_scr) + str(r_row) + str(r_col))
+        if my_digits in my_list:
+            continue
+        else:
+            my_list.append(my_digits)
 
-# r_participants = input("How many partcipants are there? ")
-# r_row = input("How many rows are there? ")
-# r_col = input("How many columns are there? ")
-my_list = list()
+    return my_list
 
-print("There are {0} participants spreade across {1} screens with {2} rows and {3} columns.".format(participants, screens, rows, cols))
-
-
-while len(my_list) < participants:
-    r_scr = random.randint(1, screens)
-    r_row = random.randint(1, rows)
-    r_col = random.randint(1, cols)
-    my_digits = int(str(r_scr) + str(r_row) + str(r_col))
-    # print(my_digits)
-    if my_digits in my_list:
-        continue
-    else:
-        my_list.append(my_digits)
-
-# print("This meetings has {0} participants.".format(len(my_list)))
-
-# print(my_list)
-
-for num in my_list:
-    if num > 999:
-        print("{0} It's a big number!".format(num))
-        print(str(num)[0][1])
-        # print("Screen: {0}, Row: {1}, Column: {2}".format(str(num)[0][1], str(num)[2], str(num)[3]))
-    else:
-        print("Screen: {0}, Row: {1}, Column: {2}".format(str(num)[0], str(num)[1], str(num)[2]))
+if __name__ == '__main__':
+    l = grid_calc(args.participants, args.rows, args.cols)
+    # print("There are {0} participants across {1} screens, {2} rows, and {3} columns".format(len(l)), "placeholder1", "placeholder2", "placeholder3")
+    for num in l:
+        if len(str(num)) > 3:
+            print("Screen: {0}, Row: {1}, Column: {2}".format(str(num)[:2],
+                                                              str(num)[2],
+                                                              str(num)[3]))
+        else:
+            print("Screen: {0}, Row: {1}, Column: {2}".format(str(num)[0], str(num)[1], str(num)[2]))

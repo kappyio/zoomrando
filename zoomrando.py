4daa41bdc7d756c5d87f112521a97497e5976323
David Kaplowitz <shredneckjazz@gmail.com>
2020-04-25 11:26:50 -0400

    tidying up

--- zoomrando.py
+++ zoomrando.py
@@ -2,18 +2,24 @@
 
 # TODO
 # Limit to n participants (500)
-# Make into a web page people can go to to use
+# optional command flag to just return n results instead of all at once
+# Make into a web interface people who don't know how to use CLI can use
 
 import argparse
 import math
 import random
 
-parser = argparse.ArgumentParser(description='Produce a list of random grid \
-                                 coordinates for participants in a zoom meeting. \
-                                 Uses Zoom\'s "Gallery View".')
-parser.add_argument('participants', type=int, help="How many participants are in the meeting?")
-parser.add_argument('rows', type=int, help="How many rows do you see in gallery view?")
-parser.add_argument('cols', type=int, help="How many columns do you see in gallery view?")
+parser = argparse.ArgumentParser(description='Produce a list of \
+    random grid coordinates for participants in a zoom meeting. \
+    Uses Zoom\'s "Gallery View".')
+parser.add_argument('participants', type=int, help="How many \
+    participants are in the meeting?")
+parser.add_argument('rows', type=int, help="How many rows do you see \
+    in gallery view?")
+parser.add_argument('cols', type=int, help="How many columns do you \
+    see in gallery view?")
+parser.add_argument('-n', type=int, help="How many results would you \
+    like?  Default is all")
 args = parser.parse_args()
 
 
@@ -30,17 +36,19 @@ def grid_calc(participants, rows, cols):
         else:
             my_list.append(my_digits)
 
-    print("There are {0} participants across {1} screens, {2} rows, and {3} columns".format(len(my_list), screens, rows, cols))
+    print(
+        "There are {0} participants across {1} screens, {2} rows, and {3} columns"
+        .format(len(my_list), screens, rows, cols))
 
     return my_list
 
 
 if __name__ == '__main__':
-    l = grid_calc(args.participants, args.rows, args.cols)
-    for num in l:
+    grid_list = grid_calc(args.participants, args.rows, args.cols)
+    for num in grid_list:
         if len(str(num)) > 3:
-            print("Screen: {0}, Row: {1}, Column: {2}".format(str(num)[:2],
-                                                              str(num)[2],
-                                                              str(num)[3]))
+            print("Screen: {0}, Row: {1}, Column: {2}"
+                  .format(str(num)[:2], str(num)[2], str(num)[3]))
         else:
-            print("Screen: {0}, Row: {1}, Column: {2}".format(str(num)[0], str(num)[1], str(num)[2]))
+            print("Screen: {0}, Row: {1}, Column: {2}"
+                  .format(str(num)[0], str(num)[1], str(num)[2]))
